f42ee53600662298e348115418d9823ba39a0778
sohn12 <gantalasohanreddy@gmail.com>
2019-11-10 07:37:59 +0530

    Added Routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,8 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-header",
+  styleUrls: ["./header.component.css"],
   templateUrl: "./header.component.html"
 })
-export class HeaderComponent {
-  @Output() featureSelcted = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelcted.emit(feature);
-  }
-}
+export class HeaderComponent {}
